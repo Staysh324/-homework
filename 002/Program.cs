991f9a77683d23201ac43f84860abb5137254ddf
Artem <proceller@yandex.ru>
2023-01-14 21:11:29 +0300

    exe 2

---
+++ 002/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("введите первое число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите второе число");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите третье число");
+int numberC = Convert.ToInt32(Console.ReadLine());
+int max = numberA;
+if (numberA > numberB && numberA > numberC)
+{Console.WriteLine($"максимальное число {numberA}");
+}
+if (numberB > numberA && numberB > numberC)
+{Console.WriteLine($"максимальное число {numberB}");
+}
+if (numberC > numberB && numberC > numberA)
+{Console.WriteLine($"максимальное число {numberC}");
+}
+    
+
+
+
+
