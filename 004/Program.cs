a8295774044a228bea6d4ecf49f0836a9c26c661
Artem <proceller@yandex.ru>
2023-01-15 06:34:26 +0300

    exe 4

---
+++ 004/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("введите число");
+int A = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+while (count <= (A-2))
+{
+      count = count + 2;
+      Console.WriteLine($"{count}");
+}
